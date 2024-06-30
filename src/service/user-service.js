be3eab399ee4c88b1dd410cabfc5d4ff288d1e00
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2024-07-01 01:56:02 +0700

    updare progress

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,25 +1,62 @@
 import { PrismaClient } from "@prisma/client";
-import { registerUserValidation } from "../validation/user-validation.js";
+import { loginUserValidation, registerUserValidation } from "../validation/user-validation.js";
 import { validate } from "../validation/validation.js";
 import { ResponseError } from "../error/response-error.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
+const prisma = new PrismaClient();
 
 const register = async (request) => {
   const user = validate(registerUserValidation, request);
 
-  // const countUser = await PrismaClient.user.count({
-  //   where: {
-  //     username: user.username,
-  //   },
-  // });
+  const countUser = await prisma.user.count({
+    where: {
+      username: user.username,
+    },
+  });
 
-  // if (countUser === 1) {
-  //   throw new ResponseError(400, "Username already Exists");
-  // }
+  if (countUser === 1) {
+    throw new ResponseError(400, "Username already Exists");
+  }
 
   user.password = await bcrypt.hash(user.password, 10);
 
-  return PrismaClient.user.create({
+  return prisma.user.create({
+    data: user,
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+};
+
+const login = async (request) => {
+  const loginRequest = validate(loginUserValidation, request);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: loginRequest.username,
+    },
+    select: {
+      username: true,
+      password: true
+    }
+  });
+
+  if (!user) {
+    throw new ResponseError(401, "Username Or Password Wrong");
+  }
+
+
+  const isPasswordValid = await bcrypt.compare(loginRequest.password, user.password);
+  if (!isPasswordValid) {
+    throw new ResponseError(401, "Username Or Password Wrong");
+  }
+
+  const token = uuid().toString();
+
+  return prisma.user.create({
     data: user,
     select: {
       username: true,
